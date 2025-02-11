4280f60ffbd071eacad951ed644db64e2da88800
Abush Natanem <abushnatanem@gmail.com>
2025-02-11 23:00:26 +0300

    basic setup

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold tracking-[1px] text-sky-600">
+        Fullstack Ecommerce website
+      </h1>
+    </div>
+  );
+}
---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+// This is our root server
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+
+// defining the configiatation
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+// TODO: DEFINING THE ROUTES
+
+app.get("/", function (req, res) {
+  res.send("This is the homepage");
+});
+
+const port = process.env.PORT || 3000;
+
+// CREATI THE RUNNING SERVER
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//TODO:  to check oue server is running we write curl localhost:8000o on the other terminal
