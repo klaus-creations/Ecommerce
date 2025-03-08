2b2b8b47c1b8fda356dd10eaaace6912f4d62d79
Abush Natanem <abushnatanem@gmail.com>
2025-03-08 15:52:28 +0300

    add some constat values

---
+++ client/src/constants/data.ts
@@ -0,0 +1,22 @@
+export const guarantees = [
+  {
+    title: "Free Shipping",
+    description: "For All Orders more than 99$",
+    logo: "car.svg",
+  },
+  {
+    title: "Secure Payment",
+    description: "We ensure For Secure Payment",
+    logo: "money.svg",
+  },
+  {
+    title: "100% Money Back",
+    description: "Money can return with 30 days",
+    logo: "handshake.svg",
+  },
+  {
+    title: "Online Support",
+    description: "24 / 7 online support",
+    logo: "message.svg",
+  },
+];
---
+++ client/src/constants/faq.ts
@@ -0,0 +1,42 @@
+export const faqs = [
+  {
+    question: "How do I place an order?",
+    answer:
+      "To place an order, simply browse our products, add items to your cart, and proceed to checkout. Follow the on-screen instructions to complete your purchase.",
+  },
+  {
+    question: "What payment methods do you accept?",
+    answer:
+      "We accept major credit cards, PayPal, and other secure payment options. You can view all available payment methods at checkout.",
+  },
+  {
+    question: "How can I track my order?",
+    answer:
+      "Once your order is shipped, we will send you a tracking number via email. You can use this number to track your package on our website or the courier’s site.",
+  },
+  {
+    question: "What is your return policy?",
+    answer:
+      "We offer a hassle-free return policy. If you're not satisfied with your purchase, you can return it within 30 days for a full refund or exchange, provided it meets our return conditions.",
+  },
+  {
+    question: "Do you offer international shipping?",
+    answer:
+      "Yes, we offer international shipping to many countries. Shipping costs and delivery times vary based on your location.",
+  },
+  {
+    question: "How do I contact customer support?",
+    answer:
+      "You can reach our customer support team via email, live chat, or phone. Visit our Contact Us page for more details.",
+  },
+  {
+    question: "Are my payment details secure?",
+    answer:
+      "Yes, we use industry-standard encryption and secure payment gateways to ensure your payment details are protected.",
+  },
+  {
+    question: "Can I change or cancel my order after placing it?",
+    answer:
+      "Orders can only be changed or canceled before they are processed. Please contact us as soon as possible if you need to modify your order.",
+  },
+];
