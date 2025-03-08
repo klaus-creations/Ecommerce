e247763857aebd95e2662f25e0dde57afce03026
Abush Natanem <abushnatanem@gmail.com>
2025-03-08 21:03:32 +0300

    add the mongodb configuration

---
+++ server/src/config/mongodb.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+import { DB_URI } from "../config/env";
+
+if (!DB_URI) {
+  throw new Error("please create your database uri inside the .env");
+}
+
+const connectDB = async function () {
+  try {
+    await mongoose.connect(DB_URI as string);
+    console.log(`database connected successfully `);
+  } catch (error) {
+    console.log(
+      "Error while trying to connect to the database",
+      error instanceof Error ? error.message : error
+    );
+    process.exit(1);
+  }
+};
+
+export default connectDB;
