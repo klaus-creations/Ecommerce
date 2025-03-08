926e962a32323e4dc2c15e5d8dc1354aa2e4d8ba
Abush Natanem <abushnatanem@gmail.com>
2025-03-08 15:53:01 +0300

    update the home page

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,17 @@
+import Daydeals from "@/components/Daydeals";
+import FaqComponent from "@/components/FaqComponent";
+import FeaturedComponent from "@/components/FeaturedComponent";
+import GuaranteeComponent from "@/components/GuaranteeComponent";
 import HeroComponent from "@/components/HeroComponent";
 
 export default function HomePage() {
   return (
     <div className="w-full">
       <HeroComponent />
+      <GuaranteeComponent />
+      <FeaturedComponent />
+      <Daydeals />
+      <FaqComponent />
     </div>
   );
 }
