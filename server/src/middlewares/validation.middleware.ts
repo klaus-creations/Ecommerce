39bc7ff44860eaa63931147150cd9ac1d5a55dca
Abush Natanem <abushnatanem@gmail.com>
2025-03-08 21:03:58 +0300

    update the validation middleware

--- server/src/middlewares/validation.middleware.ts
+++ server/src/middlewares/validation.middleware.ts
@@ -1,14 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
-export const handleValidationErrors = (
+export const handleValidationErrors = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<void> => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    res.status(400).json({ errors: errors.array() });
   }
   next();
 };
