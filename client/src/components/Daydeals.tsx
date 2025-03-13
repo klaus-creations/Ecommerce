d16bf4c7239c4a9902d449af1f9076a66e87ae27
Abush Natanem <abushnatanem@gmail.com>
2025-03-13 20:11:42 +0300

    simple change

--- client/src/components/Daydeals.tsx
+++ client/src/components/Daydeals.tsx
@@ -27,3 +27,20 @@ export default function Daydeals() {
     </div>
   );
 }
+
+// {
+
+//   Copy
+//   <Dialog>
+//     <DialogTrigger>Open</DialogTrigger>
+//     <DialogContent>
+//       <DialogHeader>
+//         <DialogTitle>Are you absolutely sure?</DialogTitle>
+//         <DialogDescription>
+//           This action cannot be undone. This will permanently delete your account
+//           and remove your data from our servers.
+//         </DialogDescription>
+//       </DialogHeader>
+//     </DialogContent>
+//   </Dialog>
+// }
