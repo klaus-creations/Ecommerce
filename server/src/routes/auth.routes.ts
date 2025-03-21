0b3cf61dcdac4a8ab5ffbdfd16529ee534068778
Abush Natanem <abushnatanem@gmail.com>
2025-03-21 17:56:07 +0300

    update auth route

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createUser,
+  isAuthenticated,
   loginUser,
   logoutUser,
 } from "../controllers/auth.controller";
@@ -9,6 +10,7 @@ import {
   signupValidation,
 } from "../validations/auth.validation";
 import { handleValidationErrors } from "../middlewares/validation.middleware";
+import { authorize } from "../middlewares/authorization.middleware";
 
 const authRouter = Router();
 
@@ -26,6 +28,7 @@ authRouter.post(
   loginUser
 );
 
-authRouter.post("/logout", logoutUser);
+authRouter.get("/logout", logoutUser);
+authRouter.get("/is-auth", authorize, isAuthenticated);
 
 export default authRouter;
