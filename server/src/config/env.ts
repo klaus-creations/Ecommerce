5ee7dd0e1c3e1dd5cac26e1b5031b60311145732
Abush Natanem <abushnatanem@gmail.com>
2025-03-21 17:56:17 +0300

    update

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,5 +1,12 @@
 import { config } from "dotenv";
 
 config({ path: `.env` });
-export const { PORT, DB_URI, JWT_SECRET, SMTP_USER, SMTP_PASS, SENDER_EMAIL } =
-  process.env;
+export const {
+  PORT,
+  DB_URI,
+  JWT_SECRET,
+  SMTP_USER,
+  SMTP_PASS,
+  SENDER_EMAIL,
+  SESSION_SECRET,
+} = process.env;
