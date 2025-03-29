1542232816bde79f67aa90f8175c4ce455611f95
Abush Natanem <abushnatanem@gmail.com>
2025-03-29 22:59:54 +0300

    setup zustand state management

--- client/src/features/theme.ts
+++ client/src/features/theme.ts
@@ -1,12 +1,25 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
+import { createJSONStorage } from "zustand/middleware";
 
-export const useTheme = create(
-  immer((set) => ({
-    isDarkMode: false,
-    setTheme: (theme: boolean) =>
-      set((state: { isDarkMode: boolean }) => {
-        state.isDarkMode = theme;
-      }),
-  }))
+interface ThemeState {
+  isDarkMode: boolean;
+  setTheme: (theme: boolean) => void;
+}
+
+export const useTheme = create<ThemeState>()(
+  persist(
+    immer((set) => ({
+      isDarkMode: false, // Default theme mode
+      setTheme: (theme: boolean) =>
+        set((state) => {
+          state.isDarkMode = theme;
+        }),
+    })),
+    {
+      name: "theme",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
 );
