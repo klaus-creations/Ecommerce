6624a67bbaa0bbf6eeae9798806b533935ea926a
Abush Natanem <abushnatanem@gmail.com>
2025-03-30 15:11:44 +0300

    update layout by addingr foote

--- client/src/pages/MainLayoutPage.tsx
+++ client/src/pages/MainLayoutPage.tsx
@@ -1,6 +1,7 @@
 import HeaderComponent from "@/components/common/HeaderComponent";
 import { Outlet } from "react-router-dom";
 import { useTheme } from "@/features/theme";
+import Footer from "@/components/common/Footer";
 
 export default function MainLayoutPage() {
   const { isDarkMode } = useTheme();
@@ -14,6 +15,7 @@ export default function MainLayoutPage() {
       <HeaderComponent />
       <main className="w-full h-[85%] bg-slate-50 dark:bg-slate-800 overflow-y-auto">
         {<Outlet />}
+        <Footer />
       </main>
     </div>
   );
