4703455187cd4cbf8a056fcd12c02f89f03bd131
Abush Natanem <abushnatanem@gmail.com>
2025-03-30 15:10:15 +0300

    footer component

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full min-h-96 bg-slate-500 dark:bg-slate-950 text-white text-6xl mt-20">
+      Footer
+    </footer>
+  );
+}
