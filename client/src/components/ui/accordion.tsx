f1ba3bcd6cdff74761fcaf26cfdea93adcf28f22
Abush Natanem <abushnatanem@gmail.com>
2025-03-31 19:57:15 +0300

    add and update new shadcn ui components

--- client/src/components/ui/accordion.tsx
+++ client/src/components/ui/accordion.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
-import { ChevronDownIcon } from "lucide-react";
+import { ChevronDownIcon, Plus } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
@@ -39,7 +39,9 @@ function AccordionTrigger({
         {...props}
       >
         {children}
-        <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        <div className="">
+          <Plus className="text-muted-foreground pointer-events-none size-5 lg:size-7 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        </div>
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   );
--- client/src/components/ui/select.tsx
+++ client/src/components/ui/select.tsx
@@ -13,18 +13,18 @@ const SelectValue = SelectPrimitive.Value;
 const SelectTrigger = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
+>(({ className, children, name = "g", ...props }, ref) => (
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-9 w-full items-center justify-between whitespace-nowrap rounded-md bg-transparent px-3 py-2 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground  focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
+      "flex items-center justify-between whitespace-nowrap rounded-md bg-transparent outline-none text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground  focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
       className
     )}
     {...props}
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronDown className="h-4 w-4 opacity-50" />
+      {name && <ChevronDown className="h-4 opacity-50" />}
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ));
