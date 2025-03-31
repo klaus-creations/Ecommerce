3be7e249dcd4fd3d549644c3a8123f7b9fec405e
Abush Natanem <abushnatanem@gmail.com>
2025-03-31 20:02:51 +0300

    add nav-bar links as constants

---
+++ client/src/constants/navbar-links.ts
@@ -0,0 +1,22 @@
+export const navbarLinks = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About",
+    link: "/about",
+  },
+  {
+    name: "Blogs",
+    link: "/blogs",
+  },
+  {
+    name: "Categories",
+    link: "/categories",
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  },
+];
