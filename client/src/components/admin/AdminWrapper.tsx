a4cd65bab548ac6e56b2b447b69d5997422e91b5
Abush Natanem <abushnatanem@gmail.com>
2025-04-01 17:53:02 +0300

    update

--- client/src/components/admin/AdminWrapper.tsx
+++ client/src/components/admin/AdminWrapper.tsx
@@ -11,9 +11,7 @@ export default function AdminWrapper({ children }: AdminWrapperProps) {
   const { isDarkMode: theme } = useTheme();
   return (
     <div className={`${theme && "dark"} w-full h-[100vh]`}>
-      <SidebarProvider
-        className={`bg-slate-50 dark:bg-slate-800 w-full h-full`}
-      >
+      <SidebarProvider className={`bg-slate-50 dark:bg-zinc-900 w-full h-full`}>
         <AppSidebar />
         <main className="size-full relative">
           <SidebarTrigger />
