0dd15934c67ff13af17bfdf277ba1da8fe69d605
Abush Natanem <abushnatanem@gmail.com>
2025-04-02 15:20:38 +0300

    update main layout page

--- client/src/pages/MainLayoutPage.tsx
+++ client/src/pages/MainLayoutPage.tsx
@@ -1,19 +1,12 @@
 import HeaderComponent from "@/components/common/HeaderComponent";
 import { Outlet } from "react-router-dom";
-import { useTheme } from "@/features/theme";
 import Footer from "@/components/common/Footer";
 
 export default function MainLayoutPage() {
-  const { isDarkMode } = useTheme();
-
   return (
-    <div
-      className={`${
-        isDarkMode && "dark"
-      } w-full h-screen  flex flex-col dark:bg-zinc-900 overflow-y-hidden scroll- text-black dark:text-white`}
-    >
+    <div className={`size-full  flex flex-col overflow-y-hidden scroll- `}>
       <HeaderComponent />
-      <main className="w-full h-[85%] bg-slate-50 dark:bg-zinc-900 overflow-y-auto">
+      <main className="w-full h-[85%] overflow-y-auto">
         {<Outlet />}
         <Footer />
       </main>
