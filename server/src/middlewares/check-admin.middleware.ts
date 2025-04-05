6cbea39afc9e09d84539c282f6ba49502b231b22
Abush Natanem <abushnatanem@gmail.com>
2025-04-05 17:23:46 +0300

    add check admin middleware

---
+++ server/src/middlewares/check-admin.middleware.ts
@@ -0,0 +1,12 @@
+// import { Request, Response, NextFunction } from "express"
+//
+//
+// const checkAdmin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+//   try {
+//
+//   } catch (error) {
+//     console.log((error as any).message || error)
+//     next(error)
+//
+//   }
+// }
