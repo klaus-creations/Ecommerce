30112e3d06c87a42bbcdbaafcc242a7e2416ec7b
Abush Natanem <abushnatanem@gmail.com>
2025-04-05 17:22:20 +0300

    add requests for products and categories

--- client/src/features/request.ts
+++ client/src/features/request.ts
@@ -1,18 +1,13 @@
 import { axiosInstanceRequest } from "./axios";
 
-export const createUser = async (userData: {
-  name: string;
-  email: string;
-  password: string;
-}) => {
+// TODO: REQUESTS FOR AUTHENTICATION 
+export const createUser = async (userData: { name: string; email: string; password: string; }) => {
   const res = await axiosInstanceRequest.post("/auth/sign-up", userData);
   return res.data;
 };
 
-export const login = async (credentials: {
-  email: string;
-  password: string;
-}) => {
+
+export const login = async (credentials: { email: string; password: string; }) => {
   const res = await axiosInstanceRequest.post("/auth/sign-in", credentials);
   return res.data;
 };
@@ -25,3 +20,31 @@ export const getSession = async () => {
 export const logoutRequest = async () => {
   await axiosInstanceRequest.get("auth/logout");
 };
+
+// TODO: REQUESTS FOR PRODUCTS
+export const getAllProducts = async () => {
+  const res = await axiosInstanceRequest.get('/products/')
+  return res?.data
+}
+
+export const createProducts = async (values: any) => {
+  console.log(values)
+  for (const pair of values.entries()) {
+    console.log(pair[0], pair[1]);
+  }
+  const res = await axiosInstanceRequest.post('/products/new', values)
+  return res.data;
+}
+
+// TODO: REQUESTS FOR CATEGORIES
+export const getAllCategories = async () => {
+
+  console.log("before the request")
+  const res = await axiosInstanceRequest.get('/categories/')
+  console.log(res.data)
+  return res.data;
+}
+
+// TODO: REQUESTS FOR REVIEWS
+
+
