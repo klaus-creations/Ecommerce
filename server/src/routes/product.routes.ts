67ca0e7b131e2bd3e48ce703b75245ae67a0673d
Abush Natanem <abushnatanem@gmail.com>
2025-04-05 17:24:03 +0300

    update route

--- server/src/routes/product.routes.ts
+++ server/src/routes/product.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import upload from "../middlewares/multer.js";
 
 // NOTE: Product Controllers
 import {
   createProduct,
   getAllProducts,
 } from "../controllers/product.controller.js";
+
+import { authorize } from "../middlewares/admin.middleware.js";
 
 // Note: Product validations
 import { productValidation } from "../validations/product.validation.js";
@@ -15,8 +18,10 @@ const productRoute = Router();
 productRoute.get("/", getAllProducts);
 productRoute.post(
   "/new",
+  upload.array("files", 6),
   productValidation,
   handleValidationErrors,
+  authorize,
   createProduct
 );
 
